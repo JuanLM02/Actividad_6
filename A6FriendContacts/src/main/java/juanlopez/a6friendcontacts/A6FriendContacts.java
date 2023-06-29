f39c1e3710e597724a30e577e0b640baa77c3620
JuanLM02 <jcamilolopezmorales@gmail.com>
2023-06-29 00:03:00 -0500

    first commit

---
+++ A6FriendContacts/src/main/java/juanlopez/a6friendcontacts/A6FriendContacts.java
@@ -0,0 +1,10 @@
+
+package juanlopez.a6friendcontacts;
+
+public class A6FriendContacts {
+
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+    }
+}
